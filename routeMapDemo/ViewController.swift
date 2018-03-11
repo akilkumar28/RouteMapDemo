c979fef47be59f9100dc3c1906fd6f5dcbfdd2ff
akilkumar28 <akil.psn@gmail.com>
2018-03-10 18:34:21 -0700

    added MKPolygon

--- routeMapDemo/ViewController.swift
+++ routeMapDemo/ViewController.swift
@@ -11,7 +11,7 @@ import MapKit
 
 
 class ViewController: UIViewController, MKMapViewDelegate {
-
+    
     @IBOutlet weak var myMapView: MKMapView!
     
     
@@ -25,67 +25,66 @@ class ViewController: UIViewController, MKMapViewDelegate {
     
     func showRouteOnMap(pickupCoordinate: CLLocationCoordinate2D, destinationCoordinate: CLLocationCoordinate2D) {
         
-        let sourcePlacemark = MKPlacemark(coordinate: pickupCoordinate, addressDictionary: nil)
-        let destinationPlacemark = MKPlacemark(coordinate: destinationCoordinate, addressDictionary: nil)
+        let cord1 = CLLocationCoordinate2D(latitude: 34.4313, longitude: -118.59890)
+        let cord2 = CLLocationCoordinate2D(latitude: 34.4274, longitude: -118.60246)
+        let cord3 = CLLocationCoordinate2D(latitude: 34.4268, longitude: -118.60181)
+        let cord4 = CLLocationCoordinate2D(latitude: 34.4202, longitude: -118.6004)
+        let coords = [cord1,cord2,cord3,cord4]
+        let myPolyline = MKPolygon(coordinates: coords, count: coords.count)
         
-        let sourceMapItem = MKMapItem(placemark: sourcePlacemark)
-        let destinationMapItem = MKMapItem(placemark: destinationPlacemark)
+        myMapView.add(myPolyline)
+        let ano1 = MKPointAnnotation()
+        ano1.coordinate = cord1
+        let ano2 = MKPointAnnotation()
+        ano2.coordinate = cord2
+        let ano3 = MKPointAnnotation()
+        ano3.coordinate = cord3
+        let ano4 = MKPointAnnotation()
+        ano4.coordinate = cord4
         
-        let sourceAnnotation = MKPointAnnotation()
+        myMapView.addAnnotations([ano1,ano2,ano3,ano4])
         
-        if let location = sourcePlacemark.location {
-            sourceAnnotation.coordinate = location.coordinate
-        }
-        
-        let destinationAnnotation = MKPointAnnotation()
-        
-        if let location = destinationPlacemark.location {
-            destinationAnnotation.coordinate = location.coordinate
-        }
-        
-        self.myMapView.showAnnotations([sourceAnnotation,destinationAnnotation], animated: true )
-        
-        let directionRequest = MKDirectionsRequest()
-        directionRequest.source = sourceMapItem
-        directionRequest.destination = destinationMapItem
-        directionRequest.transportType = .automobile
-        
-        // Calculate the direction
-        let directions = MKDirections(request: directionRequest)
-        
-        directions.calculate {
-            (response, error) -> Void in
-            
-            guard let response = response else {
-                if let error = error {
-                    print("Error: \(error)")
-                }
-                
-                return
-            }
-            
-            let route = response.routes[0]
-            
-            self.myMapView.add((route.polyline), level: MKOverlayLevel.aboveRoads)
-            
-            let rect = route.polyline.boundingMapRect
-            self.myMapView.setRegion(MKCoordinateRegionForMapRect(rect), animated: true)
-        }
+        //        let directionRequest = MKDirectionsRequest()
+        //        directionRequest.source = sourceMapItem
+        //        directionRequest.destination = destinationMapItem
+        //        directionRequest.transportType = .automobile
+        //
+        //        // Calculate the direction
+        //        let directions = MKDirections(request: directionRequest)
+        //
+        //        directions.calculate {
+        //            (response, error) -> Void in
+        //
+        //            guard let response = response else {
+        //                if let error = error {
+        //                    print("Error: \(error)")
+        //                }
+        //
+        //                return
+        //            }
+        //
+        //            let route = response.routes[0]
+        //
+        //            self.myMapView.add((route.polyline), level: MKOverlayLevel.aboveRoads)
+        //
+        //            let rect = route.polyline.boundingMapRect
+        //            self.myMapView.setRegion(MKCoordinateRegionForMapRect(rect), animated: true)
+        //        }
     }
     
     // MARK: - MKMapViewDelegate
     
     func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
         
-        let renderer = MKPolylineRenderer(overlay: overlay)
+        let renderer = MKPolygonRenderer(overlay: overlay)
         
-        renderer.strokeColor = UIColor(red: 17.0/255.0, green: 147.0/255.0, blue: 255.0/255.0, alpha: 1)
+        renderer.strokeColor = UIColor.green
         
         renderer.lineWidth = 5.0
         
         return renderer
     }
-    }
+}
 
 
 
